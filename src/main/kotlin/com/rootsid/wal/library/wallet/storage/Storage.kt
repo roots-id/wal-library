5430d9747c08014a0b26f39fd5556bd552bcc757
Rogelio Blanco <rblanco.cr.dev@gmail.com>
2022-08-27 13:38:23 -0600

    Refactor #21 - Add external persistence storage
    - Create new interfaces to store objects in the persistence layer
    - Create common interface for storage purposes
    - Restructure code to different packages

--- src/main/kotlin/com/rootsid/wal/library/wallet/storage/Storage.kt
+++
@@ -1,7 +0,0 @@
-package com.rootsid.wal.library.wallet.storage
-
-import com.rootsid.wal.library.wallet.model.Wallet
-
-interface Storage {
-    fun createWallet(name: String, seed: ByteArray): Wallet
-}
